1f275f0875e34320b89c498881498999e4dcbb38
AndresLopez13 <andresilopeza@gmail.com>
2024-07-13 18:00:33 -0500

    rest for product and category

---
+++ product_catalog/app/controllers/categories_controller.rb
@@ -0,0 +1,41 @@
+class CategoriesController < ApplicationController
+  def index
+    categories = Category.all
+    render json: categories, status: :ok
+  end
+
+  def show
+    category = Category.find(params[:id])
+    render json: category, status: :ok
+  end
+
+  def create
+    category = Category.new(category_params)
+    if category.save
+      render json: category, status: :created
+    else
+      render json: { errors: category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    category = Category.find(params[:id])
+    if category.update(category_params)
+      render json: category, status: :ok
+    else
+      render json: { errors: category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+    render json: { message: 'Category deleted' }, status: :ok
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
+end
