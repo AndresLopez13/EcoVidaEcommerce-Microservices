d276a462a2c5a6ad04a75f46a2d86a70dab1974a
Stalin <stalinrivera1980@gmail.com>
2024-07-28 23:38:49 -0500

    Pruebas unitarias del API de paypal con la integración con node

--- payment/src/__tests__/payment.controller.test.js
+++ payment/src/__tests__/payment.controller.test.js
@@ -10,19 +10,52 @@ describe('createOrder', () => {
         access_token: 'mock_access_token',
       },
     });
+
     axios.post.mockResolvedValueOnce({
       data: {
         id: 'mock_order_id',
+        status: 'CREATED'
       },
     });
+
     const req = {};
     const res = {
       json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
     };
 
     await createOrder(req, res);
 
     expect(axios.post).toHaveBeenCalledTimes(2);
-    expect(res.json).toHaveBeenCalledWith('Captured Order');
+
+    expect(axios.post).toHaveBeenCalledWith(
+      "https://api-m.sandbox.paypal.com/v1/oauth2/token",
+      expect.anything(),
+      expect.objectContaining({
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        auth: {
+          username: expect.any(String),
+          password: expect.any(String),
+        },
+      })
+    );
+
+    expect(axios.post).toHaveBeenCalledWith(
+      "https://api-m.sandbox.paypal.com/v2/checkout/orders",
+      expect.any(Object),
+      expect.objectContaining({
+        headers: {
+          Authorization: `Bearer mock_access_token`,
+        },
+      })
+    );
+
+    expect(res.json).toHaveBeenCalledWith({
+      id: 'mock_order_id',
+      status: 'CREATED'
+    });
   });
 });
